ab993026d27aa2aa847ac3906118677c84e57025
ppmm5211 <149855014+ppmm5211@users.noreply.github.com>
2023-11-09 14:42:27 +0800

    Update sh.js

--- sh.js
+++ sh.js
@@ -20,11 +20,11 @@ const my = '/index.php/App/User/Info';
 if ($request.url.indexOf(vip) != -1){
 sihai.data.status = "true";
 sihai.data.result = {
-sihai.data.code = "86";
-sihai.data.mobile = "13813813888";
-sihai.data.uid = "100006508";
-sihai.data.token = "74c171eb71039aa7e58f91e286fc5ebb";
- }
+sihai.data.code = "86"
+sihai.data.mobile = "13813813888"
+sihai.data.uid = "100006508"
+sihai.data.token = "74c171eb71039aa7e58f91e286fc5ebb"
+ };
 sihai.data.code = "0";
 }
 

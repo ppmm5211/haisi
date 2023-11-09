fea6f6aa6767bf6b95249b70c59885a9f9d352d3
ppmm5211 <149855014+ppmm5211@users.noreply.github.com>
2023-11-09 15:40:53 +0800

    Update sh.js

--- sh.js
+++ sh.js
@@ -13,24 +13,24 @@ hostname = *douyin123*
 
 ***************************************/
 
-var sihai = JSON.parse($response.body);
 const vip = '/index.php/App/Account/Login';
 const my = '/index.php/App/User/Info';
+let obj = JSON.parse($response.body);
 
 if ($request.url.indexOf(vip) != -1){
-sihai.data.status = "true";
-sihai.data.result["code"] = "86";
-sihai.data.result["mobile"] = "13813813888";
-sihai.data.result["uid"] = "100006508";
-sihai.data.result["token"] = "74c171eb71039aa7e58f91e286fc5ebb";
-sihai.data.code = "0";  
+obj.data.status = "true";
+obj.data.result["code"] = "86";
+obj.data.result["mobile"] = "13813813888";
+obj.data.result["uid"] = "100006508";
+obj.data.result["token"] = "74c171eb71039aa7e58f91e286fc5ebb";
+obj.data.code = "0";  
 }
 
 if ($request.url.indexOf(my) != -1){
-sihai.data.status = "true";
-sihai.data.result[mobile"] = "by~海思";
-sihai.data.result["no_agents"] = "1";
-sihai.data.result["expired_time"] = "1749383454";
-sihai.data.result["expired_days"] = "798";
+obj.data.status = "true";
+obj.data.result[mobile"] = "by~海思";
+obj.data.result["no_agents"] = "1";
+obj.data.result["expired_time"] = "1749383454";
+obj.data.result["expired_days"] = "798";
 }
-$done({body : JSON.stringify(sihai)});
+$done({body : JSON.stringify(obj)});

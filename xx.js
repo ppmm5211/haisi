0ab53b8432c9a8003f0b643d62a987ecbaa2659e
ppmm5211 <149855014+ppmm5211@users.noreply.github.com>
2023-11-10 09:20:25 +0800

    Update xx.js

--- xx.js
+++ xx.js
@@ -17,12 +17,13 @@ const my = "/index.php/App/User/Info";
 let obj = JSON.parse($response.body);
 
 if ($request.url.indexOf(vip) != -1){
+obj.status = true,
 obj.result = {
     "mobile": "13813813888",
     "uid": "100006508",
-    "token": "74c171eb71039aa7e58f91e286fc5ebb"
-    
-  }, 
+    "token": "74c171eb71039aa7e58f91e286fc5ebb"   
+  },
+obj.code = 0
 body = JSON.stringify(obj);
 };
 $done({body});

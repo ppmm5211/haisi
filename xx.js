0434e6e7d096462709f8316312aa0b6f771a074a
ppmm5211 <149855014+ppmm5211@users.noreply.github.com>
2023-11-10 10:23:44 +0800

    Update xx.js

--- xx.js
+++ xx.js
@@ -4,7 +4,7 @@
 
 
 ^http[s]?:\/\/.*\.douyin123\.cc\/index\.php\/App\/(Account\/Login|User\/Info|Index\/Update\/Info).*$ url script-response-body https://raw.githubusercontent.com/ppmm5211/haisi/main/xx.js
-^http[s]?:\/\/.*\.douyin123\.cc url script-request-header https://raw.githubusercontent.com/ppmm5211/haisi/main/token.js
+^http[s]?:\/\/.*\.douyin123\.cc\/index\.php\/App\/(Account\/Login|User\/Info|Host\/Info).*$ url script-request-header https://raw.githubusercontent.com/ppmm5211/haisi/main/token.js
 
 
 [mitm]

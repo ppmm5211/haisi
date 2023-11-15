36a741c5314fd2b4222f8119694e08ba77ae4579
ppmm5211 <149855014+ppmm5211@users.noreply.github.com>
2023-11-15 17:07:43 +0800

    Update xx.js

--- xx.js
+++ xx.js
@@ -36,10 +36,8 @@ obj.result = {
 body = JSON.stringify(obj);
 };
 if ($request.url.indexOf(xn) != -1){
-obj.data.data = {
-    "image" = "https://raw.githubusercontent.com/ppmm52111/QX/main/1A33EFB5-5E36-4BEC-9D3A-7AA618E3A686.jpeg",
-    "url" = "https://t.me/didi6661"
-  },
+obj.ad.["image"] = "https://raw.githubusercontent.com/ppmm52111/QX/main/1A33EFB5-5E36-4BEC-9D3A-7AA618E3A686.jpeg",
+obj.ad.["url"] = "https://t.me/didi6661"
 body = JSON.stringify(obj);
 }   
 $done({body});

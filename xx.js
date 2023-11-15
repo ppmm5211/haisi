cbea6b5b3c784141cc622bf819308102697cd58b
ppmm5211 <149855014+ppmm5211@users.noreply.github.com>
2023-11-15 17:29:34 +0800

    Update xx.js

--- xx.js
+++ xx.js
@@ -35,10 +35,5 @@ obj.result = {
   },
 body = JSON.stringify(obj);
 };
-if ($request.url.indexOf(xn) != -1){
-obj.ad.["image"] = "https://raw.githubusercontent.com/ppmm52111/QX/main/1A33EFB5-5E36-4BEC-9D3A-7AA618E3A686.jpeg",
-obj.ad.["url"] = "https://t.me/didi6661"
-body = JSON.stringify(obj);
-}   
 $done({body});
   

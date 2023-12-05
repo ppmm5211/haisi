af20959563aaa8eedeba06c836973e6000f24493
ppmm5211 <149855014+ppmm5211@users.noreply.github.com>
2023-12-05 11:18:10 +0800

    Create mgtv.js

---
+++ mgtv.js
@@ -0,0 +1,8 @@
+var headers = $request.headers;
+
+headers['x-r-l'] = '4A57E305-F51D-48C4-B014-49033043CC4D';
+
+headers['x-l-r-i'] = 'D49C8188-397F-4553-B7A1-36114CCF9083';
+
+
+$done({headers: headers})

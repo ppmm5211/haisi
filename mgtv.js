e4664f4a515610992374db9cf5f1f27c28c45484
ppmm5211 <149855014+ppmm5211@users.noreply.github.com>
2023-12-05 11:22:56 +0800

    Update mgtv.js

--- mgtv.js
+++ mgtv.js
@@ -1,6 +1,6 @@
 var headers = $request.headers;
 
-headers['x-r-l'] = '4A57E305-F51D-48C4-B014-49033043CC4D';
+headers['x-r-i'] = '4A57E305-F51D-48C4-B014-49033043CC4D';
 
 headers['x-l-r-i'] = 'D49C8188-397F-4553-B7A1-36114CCF9083';
 

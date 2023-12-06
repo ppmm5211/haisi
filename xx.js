142759ae9acd40a4bf8758a726e26d5fa87c4380
ppmm5211 <149855014+ppmm5211@users.noreply.github.com>
2023-12-06 09:44:51 +0800

    Update xx.js

--- xx.js
+++ xx.js
@@ -3,7 +3,7 @@
 [rewrite_local]
 
 
-^http[s]?:\/\/.*\.douyin123\.cc\/index\.php\/App\/(Account\/Login|User\/Info|Index\/Update).*$ url script-response-body https://raw.githubusercontent.com/ppmm5211/haisi/main/xx.js
+^http[s]?:\/\/.*\.douyin123\.cc\/index\.php\/App\/(Account\/Login|User\/Info|Index\/Update|Host\/Info).*$ url script-response-body https://raw.githubusercontent.com/ppmm5211/haisi/main/xx.js
 ^http[s]?:\/\/.*\.douyin123\.cc url script-request-header https://raw.githubusercontent.com/ppmm5211/haisi/main/token.js
 
 [mitm]
@@ -14,6 +14,7 @@ hostname = *douyin123*
 const vip = "/index.php/App/Account/Login";
 const my = "/index.php/App/User/Info";
 const xn = "/index.php/App/Index/Update";
+const zz = "/index.php/App/Host/Info";
 let obj = JSON.parse($response.body);
 
 if ($request.url.indexOf(vip) != -1){
@@ -40,6 +41,10 @@ obj.status = true,
 obj.ad.image = "https://raw.githubusercontent.com/ppmm5211/haisi/main/23ED760C-4983-4C26-B8EE-79B2BEDFDE69.jpeg",
 obj.ad.url = "https://t.me/didi6661" 
 
+body = JSON.stringify(obj);
+};
+if ($request.url.indexOf(zz) != -1){
+obj.msg = "请赞助获取最新脚本"
 body = JSON.stringify(obj);
 };
 $done({body});

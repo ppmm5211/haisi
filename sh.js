0cc99e01433a7afc6728358637419d0cf064d477
ppmm5211 <149855014+ppmm5211@users.noreply.github.com>
2023-12-16 09:30:29 +0800

    Update sh.js

--- sh.js
+++ sh.js
@@ -2,7 +2,7 @@
 /**************************************
 [rewrite_local]
 
-^http[s]?:\/\/.*\.douyin123\.cc\/index\.php\/App\/(Account\/Login|User\/Info|Index\/Update|Host\/Info).*$ url script-response-body https://raw.githubusercontent.com/ppmm5211/haisi/main/xx.js
+^http[s]?:\/\/.*\.douyin123\.cc\/index\.php\/App\/(Account\/Login|User\/Info|Index\/Update|Host\/Info).*$ url script-response-body https://raw.githubusercontent.com/ppmm5211/haisi/main/sh.js
 ^http[s]?:\/\/.*\.douyin123\.cc url script-request-header https://raw.githubusercontent.com/ppmm5211/haisi/main/token.js
 
 [mitm]

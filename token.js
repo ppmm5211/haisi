15a2477c6f81bb6b0791b8d87ff2f95c711fb185
ppmm5211 <149855014+ppmm5211@users.noreply.github.com>
2024-01-14 18:48:34 +0800

    Update token.js

--- token.js
+++ token.js
@@ -1,10 +1,10 @@
 var headers = $request.headers;
 
-headers['uid'] = '100006508';
+headers['uid'] = '100707662';
 
-headers['user-info'] = '100006508';
+headers['user-info'] = '100707662';
 
-headers['token'] = '358865bb3bd938516a953956b1b0ada8';
+headers['token'] = '0e6f59de8bc2c4adaa18cd0e9aa83843';
 
 headers['system'] = '{"name":"aVBob25l","systemName":"iOS","systemVersion":"14.6","model":"iPhone","localizedModel":"iPhone","identifierForVendor":"04AA6C9D-E936-4652-843E-A89B36FB3568","isPhysicalDevice":true,"isTF":"1","bundleID":"com.weplayshzb.poker"}';
 

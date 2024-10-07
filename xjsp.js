68e229f316f153c872c4b1359206787e9fa37329
ppmm5211 <149855014+ppmm5211@users.noreply.github.com>
2024-10-07 13:36:31 +0800

    Create xjsp.js

---
+++ xjsp.js
@@ -0,0 +1,14 @@
+/**************************************
+[rewrite_local]
+
+https://cwt98ehs.cqict.cn/vod/* url script-request-header https://raw.githubusercontent.com/ppmm5211/haisi/main/xjsp.js
+
+[mitm]
+hostname =cwt98ehs.cqict.cn
+
+***************************************/
+var headers = $request.headers;
+
+headers['X-Cookie-Auth'] = '3534663432336164303563653336636439376637323935306433643736656231';
+
+$done({headers: headers}
